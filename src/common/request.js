80f9a293dca54c15bd51d0d4513c787836ebdbfd
Alexander <alexander@MacBook-Pro-Alexander.local>
2020-02-20 15:36:17 +0200

    request heroku hosting

--- src/common/request.js
+++ src/common/request.js
@@ -1,7 +1,7 @@
 export function request(url, method, body) {
     return (
-        // fetch(`https://whispering-retreat-09608.herokuapp.com/v1/product${url}`, {
-            fetch(`http://localhost:3300/v1/product${url}`, {
+        fetch(`https://whispering-retreat-09608.herokuapp.com/v1/product${url}`, {
+            // fetch(`http://localhost:3300/v1/product${url}`, {
             method: method,
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(body)
